8f62fddfbcc35ec2daf51fc6416f116a13641816
Abel Georges <abelgr8@gmail.comgit config --global user.name Abel>
2018-04-27 10:27:41 -0400

    PORT fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var methodOverride = require("method-override");
 var app = express();
 var routes = require("./controllers/burgers_controller");
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 
 app.use(express.static('/public'));
